0d4cb57a4c117a6da1ee3bb369afcc41f50037c1
Ларри Ливингстон <Basil Smolyar>
2021-09-03 23:48:26 +0300

    Feat: add payment contract with percents calculating

--- contracts/PaymentContract.sol
+++ contracts/PaymentContract.sol
@@ -1,7 +1,44 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity 0.5.16;
 
 contract PaymentContract {
-  constructor() public {
+
+  struct Deal {
+    uint id;
+    uint startShare;
+    uint endShare;
+    uint startTime;
+    uint endTime;
+  }
+
+  Deal[] public deals;
+
+  function create (
+    uint id,
+    uint startShare,
+    uint endShare,
+    uint startTime,
+    uint endTime
+  ) public {
+    deals.push(Deal({
+      id: id,
+      startShare: startShare,
+      endShare: endShare,
+      startTime: startTime,
+      endTime: endTime
+    }));
+  }
+
+  function getShare (
+    uint id,
+    uint timeStamp
+  ) public view returns (uint) {
+    for (uint i = 0; i < deals.length; i++) {
+      if(deals[i].id == id) {
+        // Автоматически переводим в проценты с ошибкой округления в 0.01%
+        uint totalShare = (deals[i].endTime - timeStamp) / ((deals[i].endTime - deals[i].startTime) / 10000);
+        return totalShare * 100;
+      }
+    }
   }
 }
